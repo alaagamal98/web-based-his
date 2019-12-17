5003d1c158638f74c6ab4e29f43400a895c4e33f
Nada Ashraf <nadaashraf11@icloud.com>
2019-12-17 14:24:44 +0200

    Connected to mongoDB

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+// Connect to database
+// P.S create a database called "icu" first
+mongoose.connect("mongodb://localhost/icu");
+let db = mongoose.connection;
+
+// Check connection
+db.once("open", function() {
+  console.log("Connected to MongoDB");
+});
+
+// Check for DB errors
+db.on("error", function(err) {
+  console.log(err);
+});
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
