c974d9b26241a436504889bdd64e0508384789ea
Nada Ashraf <nadaashraf11@icloud.com>
2019-12-17 16:17:59 +0200

    Defined doctor schema

---
+++ models/doctor.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+// Define a schema
+const DoctorSchema = mongoose.Schema({
+  number: {
+    type: Number,
+    required: true
+  },
+  ssn: {
+    type: String,
+    required: true
+  },
+  first_name: {
+    type: String,
+    required: true
+  },
+  middle_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  salary: {
+    type: Number,
+    required: true
+  },
+  phone_number: {
+    type: [Number], // array of numbers
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  patients: {
+    type: [mongoose.Types.ObjectId],
+    ref: "Patient"
+  }
+});
+
+// Compile our model
+const Doctor = (module.exports = mongoose.model("Doctor", DoctorSchema));
