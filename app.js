83ec1de1d7a2bed14a987e4e10e03e34808457c6
Nada Ashraf <nadaashraf11@icloud.com>
2019-12-21 15:46:46 +0200

    Modified doctor schema

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const express = require("express");
 const nurses = require("./routes/nurses");
 const equipments = require("./routes/equipments");
 const mangers = require("./routes/mangers");
@@ -7,9 +8,7 @@ const doctors = require("./routes/doctors");
 const patients = require("./routes/patients");
 const medicines = require("./routes/medicines");
 const rooms = require("./routes/rooms");
-const express = require("express");
 const app = express();
-// const bodyParser = require("body-parser");
 
 // Database connection
 mongoose
@@ -27,9 +26,6 @@ app.use("/api/rooms", rooms);
 app.use("/api/patients", patients);
 app.use("/api/doctors", doctors);
 app.use("/api/medicines", medicines);
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use("/doctors", doctors);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
--- models/doctor.js
+++ models/doctor.js
@@ -1,85 +1,90 @@
-const Joi = require('joi');
-Joi.objectId= require('joi-objectid')(Joi);
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
+
 // Define a schema
-const Doctor = mongoose.model('Doctor', new mongoose.Schema({
-  // number: {
-  //   type: Number,
-  //   required: true,
-  //   unique: true
-  // },
-  ssn: {
-    type: String,
-    required: true,
-    unique: true,
-    lenght: 14
-  },
-  title:{
-    type:String,
-    required : true
-  },
-  first_name: {
-    type: String,
-    required: true,
-    minlenght: 2,
-    maxlenght: 12
-  },
-  
-  last_name: {
-    type: String,
-    required: true,
-    minlenght: 2,
-    maxlenght: 12
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  gender: {
-    type: String,
-    required: true
-  },
-  salary: {
-    type: Number,
-    required: true
-  },
-  phone_number: { //number wla string??
-    type: [{ type: Number}], // array of numbers
-    required: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlenght: 8,
-    unique:true
-  }
-}));
+const Doctor = mongoose.model(
+  "Doctor",
+  new mongoose.Schema({
+    ssn: {
+      type: String,
+      required: true,
+      unique: true,
+      length: 14
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    first_name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 12
+    },
+
+    last_name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 12
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    gender: {
+      type: String,
+      required: true
+    },
+    salary: {
+      type: Number,
+      required: true
+    },
+    phone_number: {
+      //number wla string??
+      type: [{ type: Number }], // array of numbers
+      required: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      unique: true
+    }
+  })
+);
 
 function validateDoctor(doctor) {
-  const schema = {
+  const schema = Joi.object().keys({
     //de al data ali h5lii user ed5lha
-    DoctorSsn:Joi.String().required().unique().lenght(14),
-    Doctortitle: Joi.String().required(),
-    Doctorfirst_name: Joi.String().required().minlenght(2).maxlenght(12),
-    Doctorlast_name: Joi.String().required().minlenght(2).maxlenght(12),
-    DoctorEmail: Joi.String().required(),
-    DoctorGender: Joi.String().required(),
-    DoctorSalary: Joi.Number().required(),
-    DoctorPhone_number :Joi.Number([]).required(),
-    DoctorPassword: Joi.String().required().minlenght(8).unique()
-  };
+    ssn: Joi.string()
+      .required()
+      // .unique()
+      .min(14),
+    title: Joi.string().required(),
+    first_name: Joi.string()
+      .required()
+      .min(2)
+      .max(12),
+    last_name: Joi.string()
+      .required()
+      .min(2)
+      .max(12),
+    email: Joi.string().required(),
+    gender: Joi.string().required(),
+    salary: Joi.number().required(),
+    // phone_number: Joi.number([]).required(),
+    phone_number: Joi.number().required(),
+    password: Joi.string()
+      .required()
+      .min(8)
+    // .unique()
+  });
 
   return Joi.validate(doctor, schema);
 }
 
-
-exports.Doctor = Doctor; 
-exports.validate = validateDoctor;
-
-
-// To add additional functionality to schema
-//DoctorSchema.plugin(uniqueValidator);
-
-// Compile our model
-//const Doctor = (module.exports = mongoose.model("Doctor", DoctorSchema));
+exports.Doctor = Doctor;
+exports.validateDoctor = validateDoctor;
