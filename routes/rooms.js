d50433061f0a4f5490c2d2aa53a8eafe1caf5f14
Nada Ashraf <nadaashraf11@icloud.com>
2019-12-22 14:35:44 +0200

    Added and tested room register

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,32 +1,58 @@
-
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {Room, validateRoom} = require('../models/room');
+const _ = require("lodash");
+const { Room, validateRoom } = require("../models/room");
 
+// routes
 
-// routes 
+// Add room
+router.post("/add_room", async (req, res) => {
+  // Validate The Request
+  const { error } = validateRoom(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  // Check if already exisits
+  let room = await Room.findOne({ room_number: req.body.room_number });
+  if (room) {
+    return res.status(400).send("That room already exisits!");
+  } else {
+    // Insert the new room if they do not exist yet
+    room = new Room(
+      _.pick(req.body, [
+        "room_number",
+        "vacancyOfRoom",
+        "numberOfEquipment",
+        "nameOfEquipment"
+      ])
+    );
+    await room.save();
+    res.send(room);
+  }
+});
+
 //delete
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const room = await Room.findByIdAndRemove(req.params.id);
 
-  if (!room) return res.status(404).send('The room with the given ID was not found.');
+  if (!room)
+    return res.status(404).send("The room with the given ID was not found.");
 
   res.send(room);
 });
 //............................
 
-router.get("/",async (req, res) => {
-  const rooms = await Room.find({}) ;
-  res.render('frontend page',{rooms:rooms})
+router.get("/", async (req, res) => {
+  const rooms = await Room.find({});
+  res.render("frontend page", { rooms: rooms });
   res.send(rooms);
 });
 
-router.get("/:id",async (req, res) => {
+router.get("/:id", async (req, res) => {
   const room = await Room.findById(req.params.id);
-  if (!room) return res.status(404).send("The room with the given ID was not found.");
+  if (!room)
+    return res.status(404).send("The room with the given ID was not found.");
   res.send(room);
 });
-
 
 module.exports = router;
